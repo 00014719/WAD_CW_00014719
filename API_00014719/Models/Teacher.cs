d70e89cae4fb415d9102b881e15f394c7b2f0760
00014719 <117642084+00014719@users.noreply.github.com>
2024-03-06 13:16:16 +0500

    WEBAPI project and MODELS creation

---
+++ API_00014719/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace API_00014719.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
