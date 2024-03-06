8b921a903e49b74121fc8a8d92c31f1c31f22fb9
00014719 <117642084+00014719@users.noreply.github.com>
2024-03-06 13:42:21 +0500

    MIGRATION and DBCONTEXT

---
+++ API_00014719/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using API_00014719.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_00014719.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
