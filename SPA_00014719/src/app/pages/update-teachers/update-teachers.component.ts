c634f5037b7888bae3605db1a65db868042ae8f6
00014719 <117642084+00014719@users.noreply.github.com>
2024-03-12 22:32:49 +0500

    Add styles

--- SPA_00014719/src/app/pages/update-teachers/update-teachers.component.ts
+++ SPA_00014719/src/app/pages/update-teachers/update-teachers.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Teacher } from '../../services/models/teacher.model';
+import { TeacherService } from '../../services/teacher.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-teachers',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './update-teachers.component.html',
   styleUrl: './update-teachers.component.css'
 })
-export class UpdateTeachersComponent {
+export class UpdateTeachersComponent implements OnInit {
+  teacher: Teacher = {
+    id: 0,
+    firstName: '',
+    lastName: ''
+  }
 
+  teacherId = 0;
+
+  constructor(private teacherService: TeacherService, 
+    private router: Router, 
+    private route: ActivatedRoute,){}
+
+  ngOnInit(): void {
+    const teacherId = this.route.snapshot.paramMap.get('id');
+    this.teacherId = teacherId ? + teacherId : 0;    
+  }
+
+  editTeacher(teacher: Teacher){
+    this.teacherService.updateTeacher(this.teacherId, teacher)
+      .subscribe(
+        response => {
+          this.router.navigate(['/teachers']);
+        }
+      );
+  }
 }
